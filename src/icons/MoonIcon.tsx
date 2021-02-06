8713ab9b5d16a998712dea74a1c380e4cf7cad44
Tolga Durukan <tolgadurukan16@gmail.com>
2021-02-06 22:28:36 +0200

    transformed icons to Functional Components

--- src/icons/MoonIcon.tsx
+++ src/icons/MoonIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export const MoonIcon: React.FC = () => {
+export const MoonIcon: React.VoidFunctionComponent = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26">
       <path
