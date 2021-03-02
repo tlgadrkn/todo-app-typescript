c076fb9d557ce1358fb7301f47d3fb07d84c4ba9
Tolga Durukan <tolgadurukan16@gmail.com>
2021-03-02 22:50:27 +0200

    header text fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
-import {url} from 'inspector'
 import * as React from 'react'
 import {MoonIcon} from '../icons/MoonIcon'
 import {TodoForm} from './TodoForm'
+import img from '../assets/images/bg-desktop-light.jpg'
 const Header: React.FC = () => {
   return (
-    <div className="flex-none justify-center bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 header w-screen h-64	">
-      <div className="flex flex-row mx-auto justify-between content-center items-center w-1/2 border-black border-solid p-4 ">
-        <h1 className="text-white text-5xl">TODO</h1>
+    <div
+      className="flex-none justify-center bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 header w-screen h-72 bg-no-repeat bg-cover"
+      style={{backgroundImage: `url(${img})`}}
+    >
+      <div className="flex flex-row mx-auto justify-between content-center items-center w-1/2  border-black border-solid p-4 ">
+        <h1 className="text-white text-5xl font-semibold	tracking-widest">
+          TODO
+        </h1>
         <button>
           <MoonIcon />
         </button>
