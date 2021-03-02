d55daf98315b8edefb2cb5062f23efd34cedfe61
Tolga Durukan <tolgadurukan16@gmail.com>
2021-03-02 21:50:24 +0200

    working on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import './App.css'
+// import './App.css'
 import Header from './components/Header'
 
 const App: React.FC = () => {
   return (
-    <div className="App box-border">
+    <div className="container box-border">
       <div className="container mx-auto">
         <Header />
       </div>
+      <div>body</div>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import {url} from 'inspector'
 import * as React from 'react'
 import {MoonIcon} from '../icons/MoonIcon'
 
 const Header: React.FC = () => {
   return (
-    <div className="App-header">
+    <div className="flex- bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 header">
+      <p>asdj</p>
       <MoonIcon />
     </div>
   )
